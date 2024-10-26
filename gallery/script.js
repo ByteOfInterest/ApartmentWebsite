18ff424cddbb506a5073f12703a8e56324eb9810
Anastasiia <anastasiia.zet@gmail.com>
2024-10-27 00:15:47 +0200

    Changed the gallery display to grid, added basic styles

--- gallery/script.js
+++ gallery/script.js
@@ -1,12 +1,12 @@
-// Lightbox
+// Lightbox 
 const lightbox = document.getElementById("lightbox");
 const lightboxImg = document.getElementById("lightbox-img");
 const lightboxClose = document.querySelector(".lightbox-close");
 
-document.querySelectorAll(".gallery-item img").forEach((img) => {
+document.querySelectorAll(".gallery-img").forEach((img) => {
     img.addEventListener("click", () => {
         lightboxImg.src = img.src;
-        lightbox.style.display = "flex";
+        lightbox.style.display = "flex"; 
     });
 });
 
